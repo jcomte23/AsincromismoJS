2619f23029b70b8a7707f082bc7ba04a3aa35892
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-07-20 04:36:22 -0500

    descomentacion del codigo

--- src/callback/llamadoAPI.js
+++ src/callback/llamadoAPI.js
@@ -1,79 +1,38 @@
-//variables para establecer conexion con la api
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const endPoindAPI = "https://api.escuelajs.co/api/v1"
+//variable para establecer conexion con la API
+const XMLHttpRequest = require ('xmlhttprequest').XMLHttpRequest;
+const endPoindAPI="https://api.escuelajs.co/api/v1"
 
 //funcion principal que obtendrá la informacion del producto como un objeto
-function llamadoAPI(urlAPI, callback) {
-    //inicializar un objeto de tipo XMLHttpRequest
-    let xhttp = new XMLHttpRequest()
-    //El metodo .open realiza la petición de apertura de comunicación, 
-    //el metodo puede ser 'GET' o 'POST', luego se envia la URL, 
-    //si es asincrono (true o false), usuario y contraseña. 
-    //En esta caso solo se utiliza el metodo, la url y async
-    xhttp.open('GET', urlAPI, true)
-    //en este metodo Almacena el nombre de la función que se ejecutará
-    //cuando el objeto XMLHttpRequest cambie de estado
+function llamadoAPI(urlAPI,callback) {
+    let xhttp=new XMLHttpRequest()
+    xhttp.open('GET',urlAPI,true)
     xhttp.onReadyStateChange = function (event) {
-        //el atributo readyState define el estado del objeto XMLHttpRequest
-        //0 No inicializado
-        //1 Loading
-        //2 ejecutado
-        //3 interactuando
-        //4 completado
-        if (xhttp.readyState === 4) {
-            //si la respuesta de la API es exitosa (200 Ok)
-            if (xhttp.status === 200) {
-                //se ejecuta el callback recibiendo como argumentos un objeto, 
-                //como la respuesta de la API es un texto plano, el metodo JSON.parse 
-                //tranformará este texto en un objeto.
-
-                //El atributo devuelve un DOMString que contiene la respuesta a la consulta
-                //como un texto o null si la consulta no tuvo exito o aun no ha sido completada.
-                callback(null, JSON.parse(xhttp.responseText))
-                //si la respuesta de la API no es exitosa se captura el error
-            } else {
-                //se inicializa un objeto de tipo Error donde se le envian como argumentos un mensaje
-                //de error y la URL de la API para conocer en dónde se produjo el error
-                const error = new Error('error' + urlAPI)
-                //se ejecuta el callback recibiendo como argumentos el error y null debido a que no se pudo obtener el objeto
-                return callback(error, null)
+        if (xhttp.readyState===4) {
+            if (xhttp.status===200) {
+                callback(null,JSON.parse(xhttp.responseText))
+            }else{
+                const error=new Error('error'+urlAPI)
+                return callback(error,null)
             }
         }
     }
-    //el método .send() envia la petición al servidor
     xhttp.send();
 }
 
-
-//se invoca el metodo llamadoAPI() pasandole como argumentos la varible API concatenada
-//con la cadena 'products' para acceder a la URL de la API deseada, y una función anónima
-//que recibe 2 parámetros (un objeto de error y un arreglo que almacena todos los objetos traidos por la API).
-llamadoAPI(`${endPoindAPI}/products`, function (error1, data1) {
-    //se valida si existe un error, en caso de que exista se detiene el proceso y se imprime el error
+llamadoAPI(`${endPoindAPI}/products`,function(error1,data1) {
     if (error1) {
         return console.error(error1);
     }
-    //se invoca nuevamente la función llamadoAPI con el fin de acceder a un objeto puntual
-    //del arreglo data1, se envia como parámetros la url de la API apuntando al atributo 
-    //del primer objeto de arreglo data1 y nuevamente una función anónima.
-    llamadoAPI(`${endPoindAPI}/products/${data1[0].id}`, function (error2, data2) {
-        //si en este punto se identifica un error se imprime en consola y se detiene el proceso
+    llamadoAPI(`${endPoindAPI}/products/${data1[0].id}`,function(error2,data2){
         if (error2) {
             return console.error(error2);
         }
-        //Se invoca nuevamente la funcion llamadoAPI con el fin de acceder a la categoria, se envían como parametros la url de la API con la concatenación de 'Categories' y el atributo Id de categoria del objeto data2 de la función anterior
-        //en este caso puntual se hace uso de Optional Caining el cual hace una evalucación de las propiedades de un objeto y en vez de arrojar un error devuelve undefined en caso que la propiedad no exista o sea null.
-        //igual que las anteriores e envia una funcion anonima con 2 argumentos, un objeto Error y un objeto de datos
-        llamadoAPI(`${endPoindAPI}/categories/${data2?.category?.id}`, function (error3, data3) {
-            //se valida si existe error, en caso de que exista se detiene el proceso y se imprime el error
+        llamadoAPI(`${endPoindAPI}/categories/${data2?.category?.id}`,function (error3,data3) {
             if (error2) {
                 return console.error(error3);
             }
-            //Se imprime el objeto en la posición 1 del arreglo de los objetos obtenidos en el metodo invocado inicialmente
             console.log(data1[0]);
-            //Se imprime el titulo del objeto que se consultó en la seguna invocación de la función
             console.log(data2.title);
-            //Se imprime el nombre de la categoria a la que pertenece el objeto que se consultó en la seguna invocación del método.
             console.log(data3.name);
         })
     })
